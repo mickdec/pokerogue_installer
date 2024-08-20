f6cd733f84d9f469fd6c54db8ccd8eeab0ad4383
mickdec <mickaeldecelle@hotmail.fr>
2024-08-20 11:24:30 +0200

    Update

--- updater.sh
+++ updater.sh
@@ -7,37 +7,39 @@ killall pokerogue
 killall rogueserver
 
 echo "Removing previous installation.."
-rm -Rf pokerogue_dir
-mkdir pokerogue_dir
-cd pokerogue_dir
+rm -Rf ../pokerogue_dir
+mkdir ../pokerogue_dir
+cd ../pokerogue_dir
 
 echo "Updating.."
 git clone https://github.com/pagefaultgames/pokerogue
 git clone https://github.com/pagefaultgames/rogueserver
 
-echo "Installing RogueServer.."
-cp common.go pokerogue_dir/rogueserver/
-cp register.go pokerogue_dir/rogueserver/
-sed -i -e 's/winkwink/'$INVITATION_PHRASE'/g' pokerogue_dir/rogueserver/common.go
-cp start_rogueserver.sh pokerogue_dir/rogueserver/start_rogueserver.sh
-sed -i -e 's/winkwink/'$DB_PASSWORD'/g' pokerogue_dir/rogueserver/start_rogueserver.sh
+cd pokerogue_installer
 
-cd pokerogue_dir/rogueserver
+echo "Installing RogueServer.."
+cp RES/common.go ../pokerogue_dir/rogueserver/
+cp RES/register.go ../pokerogue_dir/rogueserver/
+sed -i -e 's/winkwink/'$INVITATION_PHRASE'/g' ../pokerogue_dir/rogueserver/common.go
+cp RES/start_rogueserver.sh ../pokerogue_dir/rogueserver/start_rogueserver.sh
+sed -i -e 's/winkwink/'$DB_PASSWORD'/g' ../pokerogue_dir/rogueserver/start_rogueserver.sh
+
+cd ../pokerogue_dir/rogueserver
 go build .
-cd ../..
+cd ../../pokerogue_installer
 
 echo "Installing PokeRogue.."
-cp _env_development pokerogue_dir/pokerogue/.env.development
-sed -i --expression 's@winkwink@'$VITE_SERVER_URL'@g' pokerogue_dir/pokerogue/.env.development
-cp package.json pokerogue_dir/pokerogue/package.json
-cp start_pokerogue.sh pokerogue_dir/pokerogue/start_pokerogue.sh
+cp RES/_env_development ../pokerogue_dir/pokerogue/.env.development
+sed -i --expression 's@winkwink@'$VITE_SERVER_URL'@g' ../pokerogue_dir/pokerogue/.env.development
+cp RES/package.json ../pokerogue_dir/pokerogue/package.json
+cp RES/start_pokerogue.sh ../pokerogue_dir/pokerogue/start_pokerogue.sh
 
-cd pokerogue_dir/pokerogue
+cd ../pokerogue_dir/pokerogue
 npm install
-cd ../..
+cd ../
 
-chmod +x pokerogue_dir/rogueserver/start_rogueserver.sh
-chmod +x pokerogue_dir/pokerogue/start_pokerogue.sh
-./pokerogue_dir/rogueserver/start_rogueserver.sh &
-cd pokerogue_dir/pokerogue/
+chmod +x rogueserver/start_rogueserver.sh
+chmod +x pokerogue/start_pokerogue.sh
+./rogueserver/start_rogueserver.sh &
+cd pokerogue/
 ./start_pokerogue.sh
