ba8abbfe2a64bc108be99e5e66e79bbbb408126d
mickdec <mickaeldecelle@hotmail.fr>
2024-08-20 10:51:12 +0200

    Update

--- updater.sh
+++ updater.sh
@@ -1,6 +1,6 @@
-$DB_PASSWORD = ""
-$INVITATION_PHRASE = ""
-$VITE_SERVER_URL = ""
+DB_PASSWORD=""
+INVITATION_PHRASE=""
+VITE_SERVER_URL=""
 
 echo "Killing pokerogue.."
 killall pokerogue
@@ -18,10 +18,10 @@ git clone https://github.com/pagefaultgames/rogueserver
 echo "Installing RogueServer.."
 cp common.go rogueserver/
 cp register.go rogueserver/
-sed -i -e 's/winkwink/'$INVITATION_PHRASE'/g' rogueserver/common.go
+sed -i -e 's/winkwink/$INVITATION_PHRASE/g' rogueserver/common.go
 chmod +x start_rogueserver.sh
 cp start_pokerogue.sh rogueserver/start_rogueserver.sh
-sed -i -e 's/winkwink/'$DB_PASSWORD'/g' rogueserver/start_rogueserver.sh
+sed -i -e 's/winkwink/$DB_PASSWORD/g' rogueserver/start_rogueserver.sh
 
 cd rogueserver
 go build .
@@ -29,7 +29,7 @@ cd ..
 
 echo "Installing PokeRogue.."
 cp _env_development pokerogue/.env.development
-sed -i -e 's/winkwink/'$VITE_SERVER_URL'/g' pokerogue/.env.development
+sed -i -e 's/winkwink/$VITE_SERVER_URL/g' pokerogue/.env.development
 cp package.json pokerogue/package.json
 chmod +x start_pokerogue.sh
 cp start_pokerogue.sh pokerogue/start_pokerogue.sh
